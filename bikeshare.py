e676fad1ba2d4a07c7c910a764bfbd2bb5b8cd08
Maxime Boulet-Audet <maxime.boulet.audet@gmail.com>
2025-07-21 22:38:31 -0700

    mod:   bikeshare.py - Removed the x tick labels from the months histogram.

--- bikeshare.py
+++ bikeshare.py
@@ -42,7 +42,6 @@ def list_available_files():
 
 def get_filters(csvfiles):
     # Asks user to specify a city, month, and day to analyze.
-    #Returns:
     #   (str) city - name of the city to analyze
     #   (str) month - name of the month to filter by, or "all" to apply no month filter
     #   (str) day - name of the day of week to filter by, or "all" to apply no day filter
